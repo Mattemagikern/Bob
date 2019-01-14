0082ca35dcc3e22417926f5c6a23e4c972688cba
mattemagikern <mansgariusson@gmail.com>
2019-01-14 21:20:41 +0100

    Stuff done
    Signed-off-by: mattemagikern <mansgariusson@gmail.com>

--- src/utils/executor.go
+++ src/utils/executor.go
@@ -5,7 +5,6 @@ import (
 	"inc"
 	"os"
 	"os/exec"
-	"regexp"
 	"strings"
 	"time"
 )
@@ -30,19 +29,17 @@ func Execute(recepie string) (err error) {
 	for indx, str := range inc.Recepies[recepie].Commands {
 		inc.Recepies[recepie].Commands[indx] = strings.Trim(str, " \t")
 	}
+
 	for _, str := range inc.Recepies[recepie].Commands {
 		if err = shell(str); err != nil {
 			return err
 		}
-
 	}
 	return
 }
 
 func shell(s string) (err error) {
-	var v string
-	var boo bool
-	v, boo, _ = Substitute(s)
+	v, boo, _ := Substitute(s)
 	if !boo {
 		fmt.Println(v)
 		a := strings.Fields(v)
@@ -52,26 +49,24 @@ func shell(s string) (err error) {
 			return err.(*exec.ExitError)
 		}
 	}
-	return nil
+	return
 }
 
 func Build() (err error) {
 	errors := make(chan error)
-	obj_chan := make(chan *inc.Object_file)
-	names := make(chan string)
 	for k, v := range inc.File_tree {
 		if inc.Sf.Src.FindString(v.Path) == "" {
 			continue
 		}
 		file_name := k[:len(k)-len(inc.Build_cmd.Exstensions[2])]
-		out_path := inc.Build_cmd.Exstensions[0] + file_name + inc.Build_cmd.Exstensions[1]
-		inc.Variables["Objects"].Expression += " " + inc.Build_cmd.Exstensions[0] + file_name + inc.Build_cmd.Exstensions[1]
-		obj := inc.State[file_name+inc.Build_cmd.Exstensions[1]]
-		go build(v, out_path, obj, file_name, names, obj_chan, errors)
+		object_name := file_name + inc.Build_cmd.Exstensions[1]
+		out_path := inc.Build_cmd.Exstensions[0] + object_name
+		inc.Variables["Objects"].Expression += " " + out_path
+		obj := inc.State[object_name]
+		go build(v, out_path, obj, &inc.Variables["CFLAGS"].Expression, errors)
+		inc.State[object_name] = &inc.Object_file{out_path, inc.Variables["CFLAGS"].Expression, time.Now()}
 	}
 	for range inc.File_tree {
-		obj_file := <-obj_chan
-		inc.State[<-names] = obj_file
 		err = <-errors
 		if err != nil {
 			return
@@ -80,26 +75,16 @@ func Build() (err error) {
 	return
 }
 
-var name *regexp.Regexp = regexp.MustCompile(`($@)`)
-var path *regexp.Regexp = regexp.MustCompile(`($<)`)
-
-func build(v *inc.File, out_path string, obj *inc.Object_file, file_name string, names chan string, obj_chan chan *inc.Object_file, errors chan error) {
-	if obj == nil || obj.Timestamp.Sub(v.Timestamp) < 0 {
-		obj_chan <- &inc.Object_file{out_path, inc.Variables["CFLAGS"].Expression, time.Now()}
-		names <- file_name
+func build(v *inc.File, out_path string, obj *inc.Object_file, flags *string, errors chan error) {
+	if obj == nil || obj.Timestamp.Sub(v.Timestamp) < 0 || obj.Flags != *flags {
 		for _, j := range inc.Build_cmd.Commands {
 			j = strings.Replace(j, "$@", out_path, -1)
 			j = strings.Replace(j, "$<", v.Path, -1)
 			if err := shell(j); err != nil {
-				fmt.Println(err)
 				errors <- err
 				return
 			}
 		}
-		errors <- nil
-		return
 	}
-	names <- ""
-	obj_chan <- nil
 	errors <- nil
 }
